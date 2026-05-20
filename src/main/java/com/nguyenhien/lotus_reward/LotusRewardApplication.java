ca6884e397509870e398a8c6872521bcca93567e
nguyenhien <nguyenhien.neu.vn@gmail.com>
2026-05-20 08:46:30 +0700

    first commit

---
+++ src/main/java/com/nguyenhien/lotus_reward/LotusRewardApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenhien.lotus_reward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotusRewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotusRewardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nguyenhien/lotus_reward/LotusRewardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenhien.lotus_reward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotusRewardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
